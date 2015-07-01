4fe1ba63e45f32a9a39545dc18c779e0fa27b826
davidmarkclements <huperekchuno@googlemail.com>
2015-07-01 23:26:25 +0100

    swipe nav, initial

--- app/logic/router.js
+++ app/logic/router.js
@@ -6,9 +6,16 @@ module.exports = (routes = {}) => {
     path = path.filter(s => s.length);
     path = path[0];
 
+    ;['mood', 'results'].forEach((item) => {
+      document.body.classList.remove(item);
+    });
+
+    document.body.classList.add(path);
+
     if (routes[path] instanceof Function) {
      routes[path]();
-   }
+    }
+
   })
 
   riot.route(location.hash.split('#')[1] || 'mood');
--- app/main.js
+++ app/main.js
@@ -39,3 +39,46 @@ function tab(target) {
 
     target.classList.remove('hidden');
 }
+
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null; 
+var app = document.querySelector('app'); 
+var page = 0;                                                  
+
+function handleTouchStart(evt) {                                         
+  xDown = evt.touches[0].clientX;                                      
+  yDown = evt.touches[0].clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) return;
+
+  var xUp = evt.touches[0].clientX;                                    
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) <= Math.abs(yDiff)) return;
+  if (Math.abs(xDiff) < 15) return;
+
+  var w = Math.max(document.documentElement.clientWidth, innerWidth || 0);
+  page = (page >= 2) ? 0 : page + 1;
+
+  if (xDiff > 0) {
+    console.log('left swipe', xDiff)
+    app.style.transform = 'translateX(-' + (w * page) + 'px)';
+    return;
+  } 
+      
+  console.log('right swipe', xDiff)
+  app.style.transform = 'translateX(' + (w * page) + 'px)';
+  
+  /* reset values */
+  xDown = null;
+  yDown = null;                                             
+};
