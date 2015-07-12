5663d207f8a810464441d28e4a066b871b67f002
davidmarkclements <huperekchuno@googlemail.com>
2015-07-12 16:04:44 +0200

    tidy

--- app/views/pace-in/view.js
+++ app/views/pace-in/view.js
@@ -4,15 +4,9 @@ const chans = require('@atmos/config/chans.json')
 const {FAST, PERFECT, SLOW} = chans.pace
 
 module.exports = (scope) => {
-  scope.fast = () => {
-    sync.vote(FAST)
-  }
+  scope.fast = () => sync.vote(FAST)
 
-  scope.perfect = () => {
-    sync.vote(PERFECT)
-  }
+  scope.perfect = () => sync.vote(PERFECT)
 
-  scope.slow = () => {
-    sync.vote(SLOW)
-  }
+  scope.slow = () => sync.vote(SLOW)
 }
